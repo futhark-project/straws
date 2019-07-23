e248b486d46abb7a65c66f21e4e84cba8b98918b
iStuffs <istuffs@gmail.com>
2019-07-23 14:48:53 +0200

    feature(gulp): archive task options

--- tasks/archive.js
+++ tasks/archive.js
@@ -1,11 +1,20 @@
 const { src, dest } = require('gulp');
+const { merge } = require('lodash');
 const zip = require('gulp-zip');
 
+const {
+  ARCHIVE,
+} = require('../straws.config.json');
+
 // Archive
-function archive() {
-    return src('dist/**/*')
-        .pipe(zip(`${process.env.npm_package_name}.zip`))
-        .pipe(dest('./'));
+function archive(
+  userConfig,
+  fileName = `${process.env.npm_package_name}.zip`
+) {
+  const config = merge({}, ARCHIVE, userConfig)
+    return src(config.src)
+      .pipe(zip(fileName))
+      .pipe(dest(config.dest));
 }
 
 module.exports = archive;
