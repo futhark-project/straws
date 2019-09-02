f1a604daedd61874b9a588861f5c9d97ca821a45
iStuffs <istuffs@gmail.com>
2019-09-02 15:19:25 +0200

    perf(js): remove gulp-load-plugin dependency

--- tasks/js.js
+++ tasks/js.js
@@ -1,16 +1,15 @@
 const { src, dest } = require('gulp');
 const named = require('vinyl-named');
-const plugins = require('gulp-load-plugins');
+const gulpIf = require('gulp-if');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 
 const production = require('./helpers/mode');
 const config = require('./helpers/config');
 const webpackConfig = require('./webpack.config');
-
-/* Plugins */
-// { autoprefixer, cleanCss, htmlmin, if, imagemin, notify, plumber, sass, sassGlob, uglify, zip }
-const $ = plugins();
 
 /* Configuration */
 const {
@@ -22,9 +21,9 @@ const {
 function js() {
     return src(PATH.src + JS.entries, { sourcemaps: !production })
         .pipe(named())
-        .pipe($.plumber({ errorHandler: $.notify.onError(ERROR) }))
+        .pipe(plumber({ errorHandler: notify.onError(ERROR) }))
         .pipe(webpackStream(webpackConfig, webpack))
-        .pipe($.if(production, $.uglify()))
+        .pipe(gulpIf(production, uglify()))
         .pipe(dest(PATH.dest + JS.dest, { sourcemaps: '.' }));
 }
 
